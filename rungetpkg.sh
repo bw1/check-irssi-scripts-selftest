6e91ec240665f39b5287d311298b320596e03423
bw1 <bw1@aol.at>
2021-01-05 14:55:22 +0100

    zw

--- rungetpkg.sh
+++ rungetpkg.sh
@@ -7,3 +7,5 @@ sudo apt-get --quiet install tree > /dev/null
 sudo apt-get --quiet install libfile-slurper-perl > /dev/null
 sudo apt-get --quiet install libyaml-perl > /dev/null
 sudo apt-get --quiet install liblwp-protocol-https-perl > /dev/null
+sudo apt-get --quiet install libhtml-parser-perl > /dev/null
+sudo apt-get --quiet install libjson-pp-perl > /dev/null
