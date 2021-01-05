4a0810812782558ca2423b2fd2db8f181c28f676
bw1 <bw1@aol.at>
2021-01-05 14:00:09 +0100

    zw

---
+++ rungetpkg.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+#  get packages
+#
+sudo apt-get install -q irssi
+sudo apt-get install -q tree
+sudo apt-get install -q libfile-slurper-perl
+sudo apt-get install -q libyaml-perl
+sudo apt-get install -q liblwp-protocol-https-perl
