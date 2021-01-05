f3771cb1c78c2830a3080eade610e2c0e81c795c
bw1 <bw1@aol.at>
2021-01-05 15:05:30 +0100

    zw

--- testhelperscript.pl
+++ testhelperscript.pl
@@ -38,6 +38,7 @@ $version = do { no strict 'refs'; ${"Irssi::Script::${CURRENT_SCRIPT}::VERSION"}
 $info{version}=$version;
 $info{commands}= join " ", @commands;
 Irssi::print("Selfcheck: script version: $version", MSGLEVEL_CRAP);
+DumpFile("info.yaml", \%info);
 
 myquit('-') unless (exists $info{selfcheckcmd} );
 
@@ -46,6 +47,7 @@ Irssi::signal_register({
 	});
 Irssi::signal_add("selfcheckstop", \&myquit);
 
+Irssi::print("Selfcheck: run self check ($info{selfcheckcmd})", MSGLEVEL_CRAP);
 Irssi::command($info{selfcheckcmd});
 
 Irssi::timeout_add_once(20000, \&myquit, 'Error: timeout');
