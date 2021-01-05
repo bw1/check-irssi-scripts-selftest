260e5c839491e499d61cf5c50870f3ea0fd46efa
bw1 <bw1@aol.at>
2021-01-05 13:08:23 +0100

    init

---
+++ runselfcheck.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl
+#
+#  run self check
+#
+use strict;
+use YAML qw/DumpFile LoadFile/;
+use File::Slurper qw/read_text write_text/;
+use File::Path qw/make_path rmtree/;
+use Cwd;
+
+my $wd= getcwd(); 
+my $wp= "tmp";
+my $configp= "irssi_config";
+my $debug=0;
+
+my $startup= <<'END';
+^set ignore_signals int quit term alrm usr1 usr2
+^set use_status_window off
+^set autocreate_windows off
+^set -clear autocreate_query_level
+^set autoclose_windows off
+^set reuse_unused_windows on
+^load perl
+save
+^script exec $$^W = 1
+run "$W/selfcheckhelperscript.pl"
+END
+
+# ^set settings_autosave off
+# ^set -clear log_close_string
+# ^set -clear log_day_changed
+# ^set -clear log_open_string
+# ^set log_timestamp * 
+
+rmtree $wp;
+mkdir $wp;
+mkdir "$wp/$configp/";
+write_text("$wp/$configp/startup", $startup);
+my $t;
+$t="$wp/$configp/scripts";
+`ln -s $wd/scripts.irssi.org/scripts/ $t`; #!!
+$t="$wp/selfcheckhelperscript.pl";
+`ln -s $wd/selfcheckhelperscript.pl $t`; #!!
+
+chdir $wp;
+$ENV{CURRENT_SCRIPT}='chansearch';
+$ENV{USER}='action';
+$ENV{TERM}='vt100';
+if ( $debug > 0 ) {
+	system("irssi", "--home=$configp");
+} else {
+	`irssi --home=$configp 2>stderr.log`;
+}
+chdir $wd;
+
+
+my $info;
+if ( -e "$wp/info.yaml" ) {
+	$info= LoadFile("$wp/info.yaml");
+	my $res= $info->{selfcheckresult};
+	if ( $res eq 'ok' ) {
+		print "selfcheckresult: $res\n";
+		exit(0);
+	}
+} 
+
+print "-------------\n";
+system "cat", "$wp/info.yaml";
+print "-------------\n";
+system "cat", "$wp/selfcheck.log";
+print "-------------\n";
+system "cat", "$wp/stderr.log";
+
+exit(-1);
---
+++ selfcheckhelperscript.pl
@@ -0,0 +1,56 @@
+use strict;
+use warnings;
+use YAML qw/LoadFile DumpFile/;
+use Irssi;
+
+use vars qw($VERSION %IRSSI);
+$VERSION = '0.1';
+%IRSSI = (
+    authors     => 'bw1',
+    contact     => 'bw1@aol.at',
+    name        => 'selfcheckhelperscript',
+    description => 'helper script for self test',
+    license     => 'GPLv2',
+    url         => 'http://scripts.irssi.org/',
+    changed     => $VERSION,
+);
+
+my $CURRENT_SCRIPT = $ENV{CURRENT_SCRIPT};
+my (%info, $version, @commands);
+
+sub myquit {
+	my ( $s )=@_;
+	Irssi::print("Selfcheck: ".$s, MSGLEVEL_CRAP);
+	$info{selfcheckresult}=$s;
+	DumpFile("info.yaml", \%info);
+	Irssi::command('quit');
+}
+
+sub cmd {
+	my ($args, $server, $witem)=@_;
+	myquit( $args );
+}
+
+Irssi::command_bind($IRSSI{name},\&cmd);
+
+Irssi::print("Selfcheck: startlog", MSGLEVEL_CRAP);
+Irssi::command('^window log on selfcheck.log');
+Irssi::print("Selfcheck: loadscript ($CURRENT_SCRIPT)", MSGLEVEL_CRAP);
+Irssi::command("script load $CURRENT_SCRIPT");
+
+Irssi::print("Selfcheck: get info", MSGLEVEL_CRAP);
+%info = do { no strict 'refs'; %{"Irssi::Script::${CURRENT_SCRIPT}::IRSSI"} };
+$version = do { no strict 'refs'; ${"Irssi::Script::${CURRENT_SCRIPT}::VERSION"} };
+@commands = sort map { $_->{cmd} } grep { $_->{category} eq "Perl scripts' commands" } Irssi::commands;
+$info{version}=$version;
+$info{commands}= join " ", @commands;
+Irssi::print("Selfcheck: script version: $version", MSGLEVEL_CRAP);
+DumpFile("info.yaml", \%info);
+
+myquit('-') unless (exists $info{selfcheckcmd} );
+
+
+Irssi::print("Selfcheck: run self check ($info{selfcheckcmd})", MSGLEVEL_CRAP);
+Irssi::command($info{selfcheckcmd});
+
+Irssi::timeout_add_once(20000, \&myquit, 'Error: timeout');
