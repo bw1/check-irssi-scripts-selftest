8576d81fefef608ffcd4354cf1984609953d11b5
bw1 <bw1@aol.at>
2021-01-08 12:17:21 +0100

    shorturl

--- runselfcheck.pl
+++ runselfcheck.pl
@@ -12,6 +12,7 @@ my $wd= getcwd();
 my $wp= "tmp";
 my $configp= "irssi_config";
 my $debug=0;
+my @scripts= qw/chansearch shorturl/;
 
 my $startup= <<'END';
 ^set ignore_signals int quit term alrm usr1 usr2
@@ -32,43 +33,49 @@ END
 # ^set -clear log_open_string
 # ^set log_timestamp * 
 
-rmtree $wp;
-mkdir $wp;
-mkdir "$wp/$configp/";
-write_text("$wp/$configp/startup", $startup);
-my $t;
-$t="$wp/$configp/scripts";
-`ln -s $wd/scripts.irssi.org/scripts/ $t`; #!!
-$t="$wp/selfcheckhelperscript.pl";
-`ln -s $wd/selfcheckhelperscript.pl $t`; #!!
+my $result=0;
+foreach my $scr ( @scripts ) {
+	print "selfcheck $scr";
+	my $wp="tmp/$scr";
+	rmtree $wp;
+	make_path "$wp/$configp/";
+	write_text("$wp/$configp/startup", $startup);
+	my $t;
+	$t="$wp/$configp/scripts";
+	`ln -s $wd/scripts.irssi.org/scripts/ $t`; #!!
+	$t="$wp/selfcheckhelperscript.pl";
+	`ln -s $wd/selfcheckhelperscript.pl $t`; #!!
 
-chdir $wp;
-$ENV{CURRENT_SCRIPT}='chansearch';
-$ENV{USER}='action';
-$ENV{TERM}='vt100';
-if ( $debug > 0 ) {
-	system("irssi", "--home=$configp");
-} else {
-	`irssi --home=$configp 2>stderr.log`;
-}
-chdir $wd;
-
-
-my $info;
-if ( -e "$wp/info.yaml" ) {
-	$info= LoadFile("$wp/info.yaml");
-	my $res= $info->{selfcheckresult};
-	if ( $res eq 'ok' ) {
-		print "selfcheckresult: $res\n";
-		exit(0);
+	chdir $wp;
+	$ENV{CURRENT_SCRIPT}='chansearch';
+	$ENV{USER}='action';
+	$ENV{TERM}='vt100';
+	if ( $debug > 0 ) {
+		system("irssi", "--home=$configp");
+	} else {
+		`irssi --home=$configp 2>stderr.log`;
 	}
-} 
+	chdir $wd;
 
-print "-------------\n";
-system "cat", "$wp/info.yaml";
-print "-------------\n";
-system "cat", "$wp/selfcheck.log";
-print "-------------\n";
-system "cat", "$wp/stderr.log";
 
-exit(-1);
+	my ($info, $ires);
+	if ( -e "$wp/info.yaml" ) {
+		$info= LoadFile("$wp/info.yaml");
+		$ires= $info->{selfcheckresult};
+		if ( $ires eq 'ok' ) {
+			print " $ires\n";
+			next;
+		}
+	} 
+
+	print " $ires\n";
+	print "-------------\n";
+	system "cat", "$wp/info.yaml";
+	print "-------------\n";
+	system "cat", "$wp/selfcheck.log";
+	print "-------------\n";
+	system "cat", "$wp/stderr.log";
+	$result=-1;
+}
+
+exit($result);
