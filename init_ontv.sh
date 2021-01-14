3f2d1ebb13b97da8427767dd41dc168f2f0adf4f
bw1 <bw1@aol.at>
2021-01-14 09:10:42 +0100

    xmltv

--- init_ontv.sh
+++ init_ontv.sh
@@ -2,8 +2,22 @@
 #
 #  init for ontv2
 #
+# main
+sudo cpanm --quiet Term::ReadKey
+sudo cpanm --quiet XML::LibXML
+sudo cpanm --quiet XML::Parser
+sudo cpanm --quiet XML::TreePP
+sudo cpanm --quiet XML::Twig
+sudo cpanm --quiet XML::Writer
+sudo cpanm --quiet Date::Manip
+sudo cpanm --quiet DateTime
+sudo cpanm --quiet File::Slurp
+
+# graper
+#sudo cpanm --quiet 
 #sudo apt-get --quiet install xmltv >/dev/null
 git clone https://github.com/XMLTV/xmltv.git
+cd xmltv
 echo yes |perl Makefile.PL
 make -j4
 sudo make install
