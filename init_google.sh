abcca49e6a58f509779d2a213e2d1065123a5c10
bw1 <bw1@aol.at>
2021-01-26 13:36:40 +0100

    google

--- init_google.sh
+++ init_google.sh
@@ -3,9 +3,9 @@
 #  init for google
 #
 # main
-mkdir ~/bin
+#mkdir ~/bin
 git clone https://github.com/jarun/googler.git
 $d=$PWD
 cd ~/bin
 ln -s $PWD/googler/googler
-PATH=$PATH:~/bin
+#PATH=$PATH:~/bin
